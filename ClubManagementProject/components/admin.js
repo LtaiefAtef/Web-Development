c646494ce08cb32a7336ea6c3d73168284e6bdb2
LtaiefAtef <ltaiefatef123@gmail.com>
2025-09-26 20:23:25 +0100

    Added search mecanisme

--- ClubManagementProject/components/admin.js
+++ ClubManagementProject/components/admin.js
@@ -5,12 +5,18 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 export default function Admin({verified}) {
     const [request,setRequests] = useState(null)
+    const [status,setStatus] = useState(null)
+    const [category,setCategory] = useState(null)
+    const [orignalData,setOriginalData] = useState(null)
     const router = useRouter()
     if(!verified?.session){
         return router.back()
     }
     async function getData(){
         const result = await getRequests();
+        setStatus([...new Set(result.stmt.map((req)=>req.status))])
+        setCategory([... new Set(result.stmt.map((req)=>req.category))])
+        setOriginalData(result)
         setRequests(result)
     }
     async function acceptRequest(e){
@@ -22,6 +28,26 @@ export default function Admin({verified}) {
         const id = e.target.getAttribute("data-request-id")
         const result = await modifyRequest(id,"Declined")
         await getData()
+    }
+    function filterByCategory(e){
+        if(e.target.value === "All"){
+            setRequests(orignalData)
+        }else{
+            setRequests({success:true,stmt:request.stmt.filter((req)=>req.category === e.target.value)})
+        }
+    }
+    function filterByStatus(e) {
+        if(e.target.value === "All"){
+            setRequests(orignalData)
+        }else{
+            setRequests({success:true,stmt:orignalData.stmt.filter((req)=>req.status === e.target.value)})
+        }
+    }
+    function searchRequests(){
+        const status = document.getElementById("status").value;
+        const category = document.getElementById("category").value;
+        const query  = document.getElementById("search-requests").value.toLowerCase();
+        setRequests({success:true,stmt:orignalData.stmt.filter((req)=> (req.request.toLowerCase().includes(query) && req.status.includes(status)))})
     }
     useEffect(()=>{
         getData()
@@ -35,22 +61,24 @@ export default function Admin({verified}) {
                         <h4>What are you looking for ?</h4>
                         <div className="search-input">
                             <img src={search.src} alt="search logo" width={20} height={20} />
-                            <input className="" type="search" placeholder="Search for category,name,status,etc"/>
+                            <input className="" id="search-requests" type="search" placeholder="Search for category,name,status,etc"/>
                         </div>
                     </div>
                     <div className="search-category">
                         <h4>Category</h4>
-                        <select className="">
-                            <option value="all">All</option>
+                        <select id="category">
+                            <option value="">All</option>
+                            {category?.map((cat,index)=><option key={index} value={cat}>{cat}</option>)}
                         </select>
                     </div>
                     <div className="search-status">
                         <h4>Status</h4>
-                        <select>
-                            <option value="all">All</option>
+                        <select id="status">
+                            <option value="">All</option>
+                            {status?.map((stat,index)=><option key={index} value={stat}>{stat}</option>)}
                         </select>
                     </div>
-                    <button>SEARCH</button>
+                    <button onClick={searchRequests}>SEARCH</button>
                 </div>
                 <table>
                     <thead>
@@ -81,13 +109,13 @@ export default function Admin({verified}) {
                             {request.status === "Pending" ? <td className="decision">
                                 <button data-request-id={request.id} className="accept" onClick={(e)=>acceptRequest(e)}>Accept</button>
                                 <button data-request-id={request.id} className="decline" onClick={(e)=>declineRequest(e)}>Decline</button>
-                            </td>:<td></td>}
+                            </td>:<td>Finalized</td>}
                         </tr>)}
                     </tbody>
                     :<tfoot><tr><td colSpan={9}>{request?.error}</td></tr></tfoot>}
-                    {request?.sucess &&<tfoot>
+                    {request?.success &&<tfoot className="total_request">
                         <tr>
-                        <td colSpan={9}>Total: 2 requests</td>
+                        <td colSpan={9}>Total: {request.stmt.filter((req)=>req.status==="Pending").length} requests</td>
                         </tr>
                     </tfoot>}
                 </table>
